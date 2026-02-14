364b6981adcf4b826f04e9ed5cb19571d4a3c010
Raxpotter <raxpotter@gmail.com>
2026-02-14 20:59:29 +0530

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,34 +4,30 @@ import "./App.css";
 export default function Page() {
   const [noCount, setNoCount] = useState(0);
   const [yesPressed, setYesPressed] = useState(false);
-  const [noButtonPos, setNoButtonPos] = useState<{ top: string; left: string } | null>(null);
-
   const yesButtonSize = noCount * 20 + 16;
 
   const handleNoClick = () => {
     setNoCount(noCount + 1);
-    moveButton();
-  };
-
-  const moveButton = () => {
-    // We subtract larger margins (200x100) to ensure the button 
-    // doesn't jump partially off-screen or under the edges.
-    const x = Math.random() * (window.innerWidth - 200);
-    const y = Math.random() * (window.innerHeight - 100);
-    
-    // Explicitly adding "px" ensures the style object works in the browser
-    setNoButtonPos({ top: `${y}px`, left: `${x}px` });
   };
 
   const getNoButtonText = () => {
     const phrases = [
-      "No", "Are you sure?", "Really sure?", "Down to earth, try again!",
-      "Stubborn, not that easy!", "Surely not?", "Such a cute brat!",
-      "Dont play with Pulse!", "Want me use my fingers to make you say yes?",
-      "Phone kharab ho jayega!!", "You will whisper in my ear??",
-      "Anju, Babyy, please say yes!!", "Be a good girl and say yes!",
+      "No",
+      "Are you sure?",
+      "Really sure?",
+      "Down to earth, try again!",
+      "Stubborn, not that easy!",
+      "Surely not?",
+      "Such a cute brat!",
+      "Dont play with Pulse!",
+      "Want me use my fingers to make you say yes?",
+      "Phone kharab ho jayega!!",
+      "You will whisper in my ear??",
+      "Anju, Babyy, please say yes!!",
+      "Be a good girl and say yes!",
       "You are already mine, just admit it!!",
     ];
+
     return phrases[Math.min(noCount, phrases.length - 1)];
   };
 
@@ -40,41 +36,27 @@ export default function Page() {
       <div className="valentine-container">
         {yesPressed ? (
           <>
-            <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" alt="kisses" />
+            <img src="https://media.tenor.com/gUiu1zyxfzYAAAAi/bear-kiss-bear-kisses.gif" />
             <div className="text-container">Ok yay!!!</div>
           </>
         ) : (
           <>
             <img
-              style={{ width: "250px", height: "180px", borderRadius: "15px" }}
+              className="h-[200px]"
+              style={{ width: "400x", height: "240px" }}
               src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
-              alt="bear"
             />
             <h1 className="text-container">Anju, Baby, Will you be my Valentine?</h1>
-            <div className="buttons-wrapper">
+            <div>
               <button
-                className="yes-button"
+                className={"yes-button"}
                 style={{ fontSize: yesButtonSize }}
                 onClick={() => setYesPressed(true)}
               >
                 Yes
               </button>
 
-              <button
-                onClick={handleNoClick}
-                onMouseEnter={moveButton} // Jumps when she tries to hover
-                className="no-button"
-                style={
-                  noButtonPos 
-                    ? { 
-                        position: "fixed", 
-                        top: noButtonPos.top, 
-                        left: noButtonPos.left,
-                        zIndex: 9999 // Ensure it stays above everything
-                      } 
-                    : {}
-                }
-              >
+              <button onClick={handleNoClick} className="no-button">
                 {noCount === 0 ? "No" : getNoButtonText()}
               </button>
             </div>
