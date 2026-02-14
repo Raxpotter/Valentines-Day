4a0bc0baae0658120043fd91d9faf0555ea48fd4
Raxpotter <raxpotter@gmail.com>
2026-02-14 20:01:02 +0530

    Rahul version

--- src/App.tsx
+++ src/App.tsx
@@ -15,19 +15,17 @@ export default function Page() {
       "No",
       "Are you sure?",
       "Really sure?",
-      "Think again!",
-      "Last chance!",
+      "Down to earth, try again!",
+      "Stubborn, not that easy!",
       "Surely not?",
-      "You might regret this!",
-      "Give it another thought!",
-      "Are you absolutely certain?",
-      "This could be a mistake!",
-      "Have a heart!",
-      "Don't be so cold!",
-      "Change of heart?",
-      "Wouldn't you reconsider?",
-      "Is that your final answer?",
-      "You're breaking my heart ;(",
+      "Such a cute brat!",
+      "Dont play with Pulse!",
+      "Want me use my fingers to make you say yes?",
+      "Phone kharab ho jayega!!",
+      "You will whisper in my ear??",
+      "Anju, Babyy, please say yes!!",
+      "Be a good girl and say yes!",
+      "You are already mine, just admit it!!",
     ];
 
     return phrases[Math.min(noCount, phrases.length - 1)];
@@ -48,7 +46,7 @@ export default function Page() {
               style={{ width: "400x", height: "240px" }}
               src="https://gifdb.com/images/high/cute-love-bear-roses-ou7zho5oosxnpo6k.gif"
             />
-            <h1 className="text-container">Will you be my Valentine?</h1>
+            <h1 className="text-container">Anju, Baby, Will you be my Valentine?</h1>
             <div>
               <button
                 className={"yes-button"}
